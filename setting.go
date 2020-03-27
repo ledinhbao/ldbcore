c414d9526bfe558e18ee914c8de1367ac1ef2434
Bao, Le-Dinh <ledinhbao@me.com>
2020-03-27 10:51:58 +0700

    Update setting.go, add Set(key, value) method

--- setting.go
+++ setting.go
@@ -3,8 +3,16 @@ package ldbcore
 import "github.com/jinzhu/gorm"
 
 // Setting contains server configuration, database-based.
+//
+// Read from database, table [settings], column key & value
 type Setting struct {
 	gorm.Model
 	Key   string
 	Value string
+}
+
+// Set (key, value) used to quickly set key-value for Setting object.
+func (s *Setting) Set(key string, value string) {
+	s.Key = key
+	s.Value = value
 }
